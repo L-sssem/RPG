6de98f5d382cb466261c8180d7931322647a1454
L-sssem <adaadatwitter@gmail.com>
2026-03-27 06:52:05 +0900

    Update chestTables.ts

--- src/data/loot/chestTables.ts
+++ src/data/loot/chestTables.ts
@@ -1,1 +1,14 @@
-export {};
+export const normalChestTable = [
+  { type: "gold", chance: 45 },
+  { type: "normal_material", chance: 35 },
+  { type: "rare_material", chance: 15 },
+  { type: "accessory", chance: 5 },
+] as const;
+
+export const specialChestTable = [
+  { type: "gold", chance: 30 },
+  { type: "normal_material", chance: 20 },
+  { type: "rare_material", chance: 25 },
+  { type: "seal", chance: 20 },
+  { type: "accessory", chance: 5 },
+] as const;
