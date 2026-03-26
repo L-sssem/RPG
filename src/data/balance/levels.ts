d9d6753662fbb4e47cb01e1e62f98aff9c477a15
L-sssem <adaadatwitter@gmail.com>
2026-03-27 06:47:37 +0900

    Update levels.ts

--- src/data/balance/levels.ts
+++ src/data/balance/levels.ts
@@ -1,1 +1,5 @@
-export {};
+export const MAX_LEVEL = 100;
+
+export function getBaseExpToNextLevel(level: number): number {
+  return Math.floor(12 + (level - 1) * (level - 1) * 2.2 + (level - 1) * 8);
+}
