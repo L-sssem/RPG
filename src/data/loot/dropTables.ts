36b6625ebe61e8d147844e647dd430beb2b93bf0
L-sssem <adaadatwitter@gmail.com>
2026-03-27 06:51:45 +0900

    Update dropTables.ts

--- src/data/loot/dropTables.ts
+++ src/data/loot/dropTables.ts
@@ -1,1 +1,7 @@
-export {};
+export const enemyDropRules = {
+  normalMaterialChance: 100,
+  rareMaterialChance: 10,
+  eliteAccessoryChance: 2,
+  bossAccessoryChance: 8,
+  bossUniqueAccessoryChance: 1,
+} as const;
