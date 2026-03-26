13b63c133c6f7adcf9a8b7fee75866caba3dfae4
L-sssem <adaadatwitter@gmail.com>
2026-03-27 06:48:56 +0900

    Update seals.ts

--- src/data/balance/seals.ts
+++ src/data/balance/seals.ts
@@ -1,1 +1,27 @@
-export {};
+export const sealEffects = {
+  trainingSeal: {
+    description: "10個ごとに鍛冶屋生成時の高ランク率を微増",
+    step: 10,
+    maxStacks: 10,
+  },
+  wealthSeal: {
+    description: "5個ごとにゴールド獲得量+1%",
+    step: 5,
+    maxStacks: 50,
+  },
+  knowledgeSeal: {
+    description: "5個ごとに獲得経験値+1%",
+    step: 5,
+    maxStacks: 50,
+  },
+  explorationSeal: {
+    description: "10個ごとにレア素材ドロップ率+1%",
+    step: 10,
+    maxStacks: 20,
+  },
+  jewelSeal: {
+    description: "10個ごとにアクセドロップ率+0.5%",
+    step: 10,
+    maxStacks: 20,
+  },
+} as const;
