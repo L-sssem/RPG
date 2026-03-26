97921ab145ec66de2fa77c64561cd8bfac56f656
L-sssem <adaadatwitter@gmail.com>
2026-03-27 06:58:19 +0900

    Create accessoryTables.ts

---
+++ src/data/loot/accessoryTables.ts
@@ -0,0 +1,47 @@
+export const accessoryDropPools = {
+  tier1: [
+    "exp_ring_t1",
+    "gold_ring_t1",
+    "traveler_emblem_t1",
+    "guardian_necklace_t1",
+  ],
+  tier2: [
+    "exp_ring_t1",
+    "gold_ring_t1",
+    "traveler_emblem_t1",
+    "guardian_necklace_t1",
+    "boss_hunter_charm_t2",
+    "drop_ring_t2",
+  ],
+  tier3: [
+    "boss_hunter_charm_t2",
+    "drop_ring_t2",
+    "boss_material_medal_t3",
+    "crit_eye_necklace_t3",
+    "loot_gold_emblem_t3",
+    "rebirth_ring_unique",
+  ],
+  tier4: [
+    "boss_material_medal_t3",
+    "crit_eye_necklace_t3",
+    "loot_gold_emblem_t3",
+    "king_treasure_charm_t4",
+    "abyss_charm_unique",
+  ],
+  tier5: [
+    "king_treasure_charm_t4",
+    "golden_king_collar_unique",
+    "hunt_god_seal_unique",
+    "immortal_ring_unique",
+    "ash_ring_unique",
+  ],
+} as const;
+
+export const uniqueAccessoryIds = [
+  "rebirth_ring_unique",
+  "abyss_charm_unique",
+  "golden_king_collar_unique",
+  "hunt_god_seal_unique",
+  "immortal_ring_unique",
+  "ash_ring_unique",
+] as const;
